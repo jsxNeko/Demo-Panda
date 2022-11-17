82d25bb6d8f7784cd2d244e9314d9fdf8e59d8e8
Serenuy <dserena01@yahoo.com>
2022-11-17 11:06:59 -0600

    Update layout & server

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const buildPath = path.join(__dirname, 'build')
 
 app.use(express.static(buildPath))
 app.use(express.json())
+app.use(cors())
+
+app.post('/pet', (req,res) => {
+  const { hasPetted } = req.body;
+  if(hasPetted === true) {
+    res.json({message: "♡", image: "happypanda"});
+  } else {
+    res.json({message: "..Why did you stop? T.T", image: "sadpanda"});
+  }
+})
 
 // gets the static files from the build folder
 app.get('*', (req, res) => {
